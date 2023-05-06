55896d3d55ee77ea8cc81bf3a8c56bb58717d838
MioraRatsiresy <99732281+MioraRatsiresy@users.noreply.github.com>
2023-05-07 00:04:40 +0300

    audio

--- application/controllers/Enregistrement.php
+++ application/controllers/Enregistrement.php
@@ -1,60 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
+class Enregistrement extends CI_Controller
+{
 
-require APPPATH . 'libraries/REST_Controller.php';
-
-class Enregistrement extends REST_Controller {
-
-    public function __construct() {
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function __construct()
+    {
         parent::__construct();
-        // Charger la librairie pour envoyer des requêtes HTTP
-        $this->load->library('curl');
-        // Charger la librairie pour manipuler les fichiers
-        $this->load->helper('file');
-    }
-
-    public function index_post() {
-        // Vérifier si le fichier audio a été envoyé
-        if (!isset($_FILES['enregistrement_vocal'])) {
-            $this->response([
-                'status' => FALSE,
-                'message' => 'Aucun fichier audio envoyé'
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        }
-
-        // Récupérer le contenu du fichier audio
-        $enregistrement_vocal = file_get_contents($_FILES['enregistrement_vocal']['tmp_name']);
-
-        // Configurer l'API Google Speech-to-Text
-        $config = [
-            'languageCode' => 'fr-FR',
-            'audio' => [
-                'content' => base64_encode($enregistrement_vocal)
-            ]
-        ];
-        $headers = [
-            'Content-Type: application/json'
-        ];
-        $api_key = 'AIzaSyCdDycio8lPCUAgeiWie3-kBvrorZOGXv4'; // Remplacer avec votre clé API Google Speech-to-Text
-
-        // Envoyer la requête à l'API Google Speech-to-Text
-        $url = 'https://speech.googleapis.com/v1/speech:recognize?key=' . $api_key;
-        $response = $this->curl->simple_post($url, json_encode($config), $headers);
-
-        // Récupérer le texte transcrit de la réponse de l'API
-        $data = json_decode($response, TRUE);
-        $transcription = '';
-        if (isset($data['results'])) {
-            foreach ($data['results'] as $result) {
-                $transcription .= $result['alternatives'][0]['transcript'] . ' ';
-            }
-        }
-
-        // Envoyer la réponse au client
-        $this->response([
-            'status' => TRUE,
-            'message' => 'Fichier audio transcrit avec succès',
-            'transcription' => $transcription
-        ], REST_Controller::HTTP_OK);
+        $this->load->helper('url');
+        $this->load->library('session');
     }
 }
--- application/views/test.php
+++ application/views/test.php
@@ -1,45 +1,46 @@
 <audio id="enregistrement_vocal" controls></audio>
 
 <button id="bouton_enregistrer">Enregistrer</button>
-
 <script>
   navigator.mediaDevices.getUserMedia({ audio: true })
-    .then(stream => {
-      const mediaRecorder = new MediaRecorder(stream);
-      const chunks = [];
+  .then(stream => {
+    const mediaRecorder = new MediaRecorder(stream);
+    const chunks = [];
 
-      mediaRecorder.addEventListener('dataavailable', event => {
-        chunks.push(event.data);
-        const enregistrementVocal = document.querySelector('#enregistrement_vocal');
-        enregistrementVocal.src = URL.createObjectURL(new Blob(chunks, { type: 'audio/ogg; codecs=opus' }));
-      });
-
-      mediaRecorder.addEventListener('stop', () => {
-        const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
-        const formData = new FormData();
-        formData.append('enregistrer_vocal', blob, 'enregistrement.ogg');
-        fetch('<?php echo base_url('transcrire_audio'); ?>', {
-          method: 'POST',
-          body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Transcription :', data.transcription);
-        });
-      });
-
-      const button = document.querySelector('#bouton_enregistrer');
-      button.addEventListener('click', () => {
-        if (mediaRecorder.state === 'inactive') {
-          mediaRecorder.start();
-          button.textContent = 'Arrêter';
-        } else {
-          mediaRecorder.stop();
-          button.textContent = 'Enregistrer';
-        }
-      });
-    })
-    .catch(error => {
-      console.error('Erreur lors de l\'accès au microphone :', error);
+    mediaRecorder.addEventListener('dataavailable', event => {
+      chunks.push(event.data);
+      const enregistrementVocal = document.querySelector('#enregistrement_vocal');
+      enregistrementVocal.src = URL.createObjectURL(new Blob(chunks, { type: 'audio/ogg; codecs=opus' }));
     });
+
+    mediaRecorder.addEventListener('stop', () => {
+      const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
+      const formData = new FormData();
+      formData.append('enregistrement_vocal', blob, 'enregistrement.ogg');
+
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log('Transcription :', this.responseText);
+        }
+      };
+      xhr.open('POST', <?php echo base_url('Enregistrement/transcrire_audio')?>);
+      xhr.send(formData);
+    });
+
+    const button = document.querySelector('#bouton_enregistrer');
+    button.addEventListener('click', () => {
+      if (mediaRecorder.state === 'inactive') {
+        mediaRecorder.start();
+        button.textContent = 'Arrêter';
+      } else {
+        mediaRecorder.stop();
+        button.textContent = 'Enregistrer';
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Erreur lors de l\'accès au microphone :', error);
+  });
+
 </script>
