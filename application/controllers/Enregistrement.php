4bf7206713b3a891dc1f058c2a8fd3455cc001a7
MioraRatsiresy <99732281+MioraRatsiresy@users.noreply.github.com>
2023-05-06 23:36:40 +0300

    update

---
+++ application/controllers/Enregistrement.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+
+class Enregistrement extends REST_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        // Charger la librairie pour envoyer des requêtes HTTP
+        $this->load->library('curl');
+        // Charger la librairie pour manipuler les fichiers
+        $this->load->helper('file');
+    }
+
+    public function index_post() {
+        // Vérifier si le fichier audio a été envoyé
+        if (!isset($_FILES['enregistrement_vocal'])) {
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Aucun fichier audio envoyé'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+
+        // Récupérer le contenu du fichier audio
+        $enregistrement_vocal = file_get_contents($_FILES['enregistrement_vocal']['tmp_name']);
+
+        // Configurer l'API Google Speech-to-Text
+        $config = [
+            'languageCode' => 'fr-FR',
+            'audio' => [
+                'content' => base64_encode($enregistrement_vocal)
+            ]
+        ];
+        $headers = [
+            'Content-Type: application/json'
+        ];
+        $api_key = 'YOUR_API_KEY_HERE'; // Remplacer avec votre clé API Google Speech-to-Text
+
+        // Envoyer la requête à l'API Google Speech-to-Text
+        $url = 'https://speech.googleapis.com/v1/speech:recognize?key=' . $api_key;
+        $response = $this->curl->simple_post($url, json_encode($config), $headers);
+
+        // Récupérer le texte transcrit de la réponse de l'API
+        $data = json_decode($response, TRUE);
+        $transcription = '';
+        if (isset($data['results'])) {
+            foreach ($data['results'] as $result) {
+                $transcription .= $result['alternatives'][0]['transcript'] . ' ';
+            }
+        }
+
+        // Envoyer la réponse au client
+        $this->response([
+            'status' => TRUE,
+            'message' => 'Fichier audio transcrit avec succès',
+            'transcription' => $transcription
+        ], REST_Controller::HTTP_OK);
+    }
+}
--- application/views/test.php
+++ application/views/test.php
@@ -1,60 +1,45 @@
-<form>
-  <button id="bouton_enregistrer">Enregistrer un message vocal</button>
-</form>
+<audio id="enregistrement_vocal" controls></audio>
 
-<audio id="audio_preview" controls></audio>
+<button id="bouton_enregistrer">Enregistrer</button>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-  $(document).ready(() => {
-    const button = $('#bouton_enregistrer');
-    const audioPreview = $('#audio_preview');
-    const chunks = [];
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      const mediaRecorder = new MediaRecorder(stream);
+      const chunks = [];
 
-    button.click(() => {
-      if (button.text() === 'Enregistrer un message vocal') {
-        navigator.mediaDevices.getUserMedia({ audio: true })
-          .then(stream => {
-            const mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.addEventListener('dataavailable', event => {
-              chunks.push(event.data);
-            });
-            mediaRecorder.addEventListener('stop', () => {
-              const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
-              const formData = new FormData();
-              formData.append('message_vocal', blob, 'message.ogg');
-              $.ajax({
-                url: '<?php echo base_url('enregistrer_message_vocal'); ?>',
-                method: 'POST',
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: () => {
-                  alert('Message vocal enregistré avec succès !');
-                },
-                error: (jqXHR, textStatus, errorThrown) => {
-                  alert(`Erreur lors de l'enregistrement du message vocal : ${errorThrown}`);
-                }
-              });
-              chunks.length = 0;
-            });
-            mediaRecorder.start();
-            button.text('Arrêter l'enregistrement');
-          })
-          .catch(error => {
-            console.error('Erreur lors de l\'accès au microphone :', error);
-          });
-      } else {
-        button.prop('disabled', true);
-        const mediaRecorder = chunks.length > 0 ? new MediaRecorder(new Blob(chunks, { type: 'audio/ogg; codecs=opus' })) : null;
-        chunks.length = 0;
-        if (mediaRecorder !== null) {
+      mediaRecorder.addEventListener('dataavailable', event => {
+        chunks.push(event.data);
+        const enregistrementVocal = document.querySelector('#enregistrement_vocal');
+        enregistrementVocal.src = URL.createObjectURL(new Blob(chunks, { type: 'audio/ogg; codecs=opus' }));
+      });
+
+      mediaRecorder.addEventListener('stop', () => {
+        const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
+        const formData = new FormData();
+        formData.append('enregistrer_vocal', blob, 'enregistrement.ogg');
+        fetch('<?php echo base_url('transcrire_audio'); ?>', {
+          method: 'POST',
+          body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Transcription :', data.transcription);
+        });
+      });
+
+      const button = document.querySelector('#bouton_enregistrer');
+      button.addEventListener('click', () => {
+        if (mediaRecorder.state === 'inactive') {
+          mediaRecorder.start();
+          button.textContent = 'Arrêter';
+        } else {
           mediaRecorder.stop();
-          audioPreview.attr('src', URL.createObjectURL(mediaRecorder.getBlob()));
+          button.textContent = 'Enregistrer';
         }
-        button.text('Enregistrer un nouveau message vocal');
-        button.prop('disabled', false);
-      }
+      });
+    })
+    .catch(error => {
+      console.error('Erreur lors de l\'accès au microphone :', error);
     });
-  });
 </script>
